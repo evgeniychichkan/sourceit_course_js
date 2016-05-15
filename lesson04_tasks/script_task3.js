dabe4910beb846c12fee32e3af75168077eceda5
Evgeniy <chichkan.evgeniy@gmail.com>
2016-05-16 01:02:48 +0300

    lesson_4 tasks

---
+++ lesson04_tasks/script_task3.js
@@ -0,0 +1,20 @@
+//Задача 3:
+
+var arr = [1,2,3,4,5,6,77,3,5];
+
+//В данном массиве заменить на null все четные элементы (которые делятся на 2 нацело)
+
+
+console.log('Массив до: \n');
+console.log(arr);
+
+var arrAmount =  arr.length;
+for (var i = 0; i < arrAmount; i++) {
+    if (arr[i] % 2 == 0) {
+       arr[i] = null;
+       console.log('Index of null = ' + i);
+  }
+}
+
+console.log('\nМассив после: \n');
+console.log(arr);
